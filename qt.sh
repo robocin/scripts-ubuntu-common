3f9dbef7a518ad0b0c23cadc2face999363b65e8
José Cruz <jvsc@cin.ufpe.br>
2021-07-04 19:29:29 -0300

    refactor(qt.sh): changes to 6, adds chown

--- qt.sh
+++ qt.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 QT=${1}
+DIR=${2}
+
+CURRENT_USER=$(who | awk 'NR==1{print $1}')
 
 if [ -z "${QT}" ]; then
-  QT="5.15.2"
+  QT="6.2.0"
+fi
+
+if [ -z "${DIR}" ]; then
+  DIR="/opt/qt"
 fi
 
 apt-get install git cmake python3 python3-pip build-essential libdbus-1-3 libpulse-mainloop-glib0 -y
+apt-get install libssl-dev x11-xkb-utils xkb-data libxcb-xkb-dev libxkbcommon-dev -y
 
 pip3 install aqtinstall
 
-aqt install --outputdir /opt/qt ${QT} linux desktop
+aqt install --outputdir "${DIR}" "${QT}" linux desktop
+
+chown "${CURRENT_USER}":"${CURRENT_USER}" "${DIR}" -R
