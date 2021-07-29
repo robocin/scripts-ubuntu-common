71daa294e4cedbee3e53991b6a8980951182bc42
José Cruz <jvsc@cin.ufpe.br>
2021-07-29 19:25:23 -0300

    refactor(qt.sh): adds vulkan libs

--- qt.sh
+++ qt.sh
@@ -15,6 +15,7 @@ fi
 
 apt-get install git cmake python3 python3-pip build-essential libdbus-1-3 libpulse-mainloop-glib0 -y
 apt-get install libssl-dev x11-xkb-utils xkb-data libxcb-xkb-dev libxkbcommon-dev -y
+apt-get install libvulkan1 mesa-vulkan-drivers vulkan-utils -y
 
 pip3 install aqtinstall
 
