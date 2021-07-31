77be158196904cdf93bb13d83bff18e3d2135f22
José Cruz <jvsc@cin.ufpe.br>
2021-07-30 23:41:02 -0300

    feat(libtorch.sh): adds the script again, puts in /opt folder by default

---
+++ libtorch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PARENT_DIR=${1}
+
+CURRENT_USER=$(who | awk 'NR==1{print $1}')
+
+if [ -z "${PARENT_DIR}" ]; then
+  PARENT_DIR="/opt"
+fi
+
+DIR="${PARENT_DIR}/libtorch"
+OUTPUT_FILE="${PARENT_DIR}/libtorch.zip"
+
+LINK="https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.9.0%2Bcpu.zip"
+
+mkdir -p "${PARENT_DIR}"
+wget -O "${OUTPUT_FILE}" "${LINK}"
+rm -f -r "${DIR}" # removes the old folder, if exists.
+unzip -o "${OUTPUT_FILE}" -d "${PARENT_DIR}"
+rm -f "${OUTPUT_FILE}"
+
+chown "${CURRENT_USER}":"${CURRENT_USER}" "${DIR}" -R
