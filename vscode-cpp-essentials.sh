82a608db35e54f58ec256f9effe049650039dda2
José Cruz <jvsc@cin.ufpe.br>
2021-10-31 20:21:52 -0300

    fix(vscode-cpp-essentials): fixes installation verification, adds todo-highlight, doxdocgen

--- vscode-cpp-essentials.sh
+++ vscode-cpp-essentials.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-if [ -z $(which codea) ]; then
-	echo -e "\x1B[31m[ERROR] This script requires Visual Studio Code installed.\n"
-	echo -e "\033[0mYou can install using snap with the following command:\n"
+if [ -z $(which code) ]; then
+  echo -e "\x1B[31m[ERROR] This script requires Visual Studio Code installed.\n"
+  echo -e "\033[0mYou can install using snap with the following command:\n"
   echo -e "\x1B[01;93m   $ snap install --classic code\n"
 else
-	# todo: static check
-	# todo: new class template
-	code --install-extension xaver.clang-format --force
-	code --install-extension ms-vscode.cpptools --force
-	code --install-extension ms-vscode.cpptools-extension-pack --force
-	code --install-extension llvm-vs-code-extensions.vscode-clangd --force
-	code --install-extension streetsidesoftware.code-spell-checker --force
-	code --install-extension eamodio.gitlens --force
+  code --install-extension xaver.clang-format --force
+  code --install-extension ms-vscode.cpptools --force
+  code --install-extension ms-vscode.cpptools-extension-pack --force
+  code --install-extension llvm-vs-code-extensions.vscode-clangd --force
+  code --install-extension streetsidesoftware.code-spell-checker --force
+  code --install-extension eamodio.gitlens --force
+  code --install-extension wayou.vscode-todo-highlight --force
+  code --install-extension cschlosser.doxdocgen --force
+  code --install-extension akiramiyakoda.cppincludeguard --force
+  # todo: new class template
+  # todo: static check
 fi
