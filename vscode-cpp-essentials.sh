d1a3373772e1ad5e00bab8dbc983d7d1791740ef
José Cruz <jvsc@cin.ufpe.br>
2021-10-31 19:53:16 -0300

    refactor(vscode-cpp-essentials.sh): fixes extensions

--- vscode-cpp-essentials.sh
+++ vscode-cpp-essentials.sh
@@ -1,34 +1,16 @@
 #!/bin/bash
 
-# PS: this script assumes a 64-bit Linux Ubuntu system with pip3 installed.
-
-# install "C/C++" vs code extension
-wget https://github.com/microsoft/vscode-cpptools/releases/download/1.2.1/cpptools-linux.vsix
-code --install-extension cpptools-linux.vsix
-rm cpptools-linux.vsix
-# PS: if this is not working, check if the default formatter is set to "C/C++" by doing a right click > "format document with..." > "C/C++"
-
-# install "C/C++ Advanced Lint" vs code extension
-apt-get install clang cppcheck -y
-pip3 install flawfinder
-pip3 install lizard
-wget https://github.com/jbenden/vscode-c-cpp-flylint/releases/download/v1.8.2/c-cpp-flylint-1.8.2.vsix
-code --install-extension c-cpp-flylint-1.8.2.vsix
-rm c-cpp-flylint-1.8.2.vsix
-
-# install "Open in External App" vs code extension
-wget https://github.com/tjx666/open-in-external-app/releases/download/V0.1.0/open-in-external-app-0.1.0.vsix
-code --install-extension open-in-external-app-0.1.0.vsix
-rm open-in-external-app-0.1.0.vsix
-
-echo "
-
-	Done!
-
-	To enable automatic formatting on save:
-	1 - right click on the code > 'format document with...' > 'C/C++'.
-	2 - inside vs code options, search for 'format on save' and make sure the checkbox is enabled.
-
-	Happy Coding!
-
-"
+if [ -z $(which codea) ]; then
+	echo -e "\x1B[31m[ERROR] This script requires Visual Studio Code installed.\n"
+	echo -e "\033[0mYou can install using snap with the following command:\n"
+  echo -e "\x1B[01;93m   $ sudo snap install --classic code\n"
+else
+	# todo: static check
+	# todo: new class template
+	code --install-extension xaver.clang-format --force
+	code --install-extension ms-vscode.cpptools --force
+	code --install-extension ms-vscode.cpptools-extension-pack --force
+	code --install-extension llvm-vs-code-extensions.vscode-clangd --force
+	code --install-extension streetsidesoftware.code-spell-checker --force
+	code --install-extension eamodio.gitlens --force
+fi
