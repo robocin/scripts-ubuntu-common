2bbfce18097a34212795b97687b7b74e3562ea91
Riei-Joaquim <rjmr@cin.ufpe.br>
2023-10-22 21:19:57 -0300

    feat: adding guard to correct privilege

--- netplan.sh
+++ netplan.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+function is_root {
+  [ "${EUID:-$(id -u)}" -eq 0 ];
+}
+
+if ! is_root; then
+  echo -e "\x1B[31m[ERROR] This script requires root privileges."
+  exit 1
+fi
+
 apt-get install netplan.io -y
